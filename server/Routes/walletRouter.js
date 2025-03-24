d25d6cbd6556cca2317e31eb7bec549a1c6a9d65
shirin261196 <shirin261196@gmail.com>
2025-03-24 23:11:04 +0530

    Add files via upload

--- server/Routes/walletRouter.js
+++ server/Routes/walletRouter.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { creditWallet, debitWallet, getWalletDetails, purchaseUsingWallet } from '../controllers/walletController.js';
+import { userAuth } from '../middleware/userAuth.js';
 
 const walletRouter = express.Router();
 
@@ -7,7 +8,7 @@ const walletRouter = express.Router();
 walletRouter.post('/wallet/credit', creditWallet );
 walletRouter.post('/wallet/debit', debitWallet );
 walletRouter.post('/wallet/purchase', purchaseUsingWallet );
-walletRouter.get('/wallet/details',getWalletDetails)
+walletRouter.get('/wallet/details',userAuth,getWalletDetails)
 
 
 export default walletRouter;
