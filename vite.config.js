e5b62c3283800eb8cb84625df1a21acbbe62132d
shirin261196 <shirin261196@gmail.com>
2025-03-26 10:05:19 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{port:5173}
-    // Only set base path for production (GitHub Pages)
-  base: process.env.NODE_ENV === 'production' ? '/ecommerceproject/' : '/',
-})
+  server: { port: 5173 },
+  base: '/ecommerceproject/', // Correct for GitHub Pages
+  build: {
+    outDir: 'dist',
+  }
+});
