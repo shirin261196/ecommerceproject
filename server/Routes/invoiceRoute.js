9f1dc50a30d6feb856653c40f95849ad42b8f4d8
shirin261196 <shirin261196@gmail.com>
2025-03-26 11:42:40 +0530

    Delete server directory

--- server/Routes/invoiceRoute.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { generateInvoice } from "../controllers/invoiceController.js";
-
-const invoiceRouter = express.Router();
-
-invoiceRouter.get("/invoice/:orderId", generateInvoice);
-
-export default invoiceRouter;
