72052966a3281e7f70779056bfb63a46b47ce259
shirin261196 <shirin261196@gmail.com>
2025-03-26 06:48:28 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server:{port:5173}
-  
+    // Only set base path for production (GitHub Pages)
+  base: process.env.NODE_ENV === 'production' ? '/ecommerceproject/' : '/',
 })
